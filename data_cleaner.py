5aaef521aceb8ce0c05fecc479e489af695c6033
Sebastian Montero <smontero@mightywizards.com>
2019-01-26 20:12:18 -0600

    Added regular expressions for common terms to eliminate

--- data_cleaner.py
+++ data_cleaner.py
@@ -12,11 +12,24 @@ class DataCleaner(object):
         return name.strip(' .,/').lower()
 
     def _remove_common_terms(self, name):
-        terms = [r'\s*s\.?a\.?\s*de\s*c\.?v\.?']
+        """ s de rl de cv """
+        """ sa[v,api,b] de cv """
+        """ s en nc de cv """
+        """de cv """
+        """sa """
+        terms = [
+            r'\s*(cia)?(compa.?ia)?(\s*s[\.\s]?(c[\.\s]?)?(p[\.\s]?r[\.\s]?)?)?\s*(de?)?\s*r[\.\s]?l[\.\s]?\s*de?\s*c[\.\s]?v[\.\s]?',
+            r'\s*(cia)?(compa.?ia)?\s*s[\.\s]?[acs][\.\s]?(b[\.\s]?)?(p[\.\s]?i[\.\s]?)?(v[\.\s]?)?\s*de?\s*c[\.\s]?v[\.\s]?',
+            r'\s*(cia)?(compa.?ia)?\s*(s[\.\s]?)?(en)?\s*n[\.\s]?c[\.\s]?\s*de?\s*c[\.\s]?v[\.\s]?',
+            r'\s*(cia)?(compa.?ia)?\s+de?\s+c[\.\s]?v[\.\s]?\b',
+            r'\b(cia)?(compa.?ia)?\bs[\.\s]?a[\.\s]?\b',
+            r'\bfidei?c?(omiso)?\b',
+            r'\b(gpo|grupo)\s*finan?(ciero)?\b'
+        ]
 
         for term in terms:
             name = re.sub(term, '', name)
-        return name
+        return name.strip()
 
     def _process_name(self, name):
         return self._remove_common_terms(self._clean_name(name))
@@ -29,28 +42,37 @@ class DataCleaner(object):
                    "from internos ")
         return conn.execute(sql).fetchall()
 
-    def _update_internal_clients(self, conn, num_cliente, nombre, grupo, agrupador):
+    def _update_internal_clients(self, conn, updates):
         sql = text("update internos set "
                    "nombre_p = :nombre, "
                    "grupo_p = :grupo, "
-                   "agrupador = :agrupador "
+                   "agrupador_p = :agrupador "
                    "where num_cliente = :num_cliente ")
-        return conn.execute(sql, num_cliente=num_cliente, nombre=nombre, grupo=grupo, agrupador=agrupador)
+        return conn.execute(sql, updates)
 
     def _clean_internal_clients(self, conn):
+        print('Cleaning internal clients...')
         internal = self._load_raw_internal_clients(conn)
+        updates = []
         for row in internal:
             nombre = self._process_name(row['nombre'])
             nombre = re.sub(r'^agrupador:?\s*', '', nombre)
             grupo = self._process_name(row['grupo'])
             agrupador = self._process_name(row['agrupador'])
-            if agrupador == 'por clasificar':
-                if grupo == 'por clasificar':
-                    agrupador = grupo = nombre
-                else:
-                    agrupador = grupo
-            self._update_internal_clients(
-                conn, row['num_cliente'], nombre, grupo, agrupador)
+
+            if grupo == 'por clasificar' or grupo == '':
+                grupo = nombre
+            if agrupador == 'por clasificar' or agrupador == '':
+                agrupador = grupo
+            updates.append({
+                'num_cliente': row['num_cliente'],
+                'nombre': nombre,
+                'grupo': grupo,
+                'agrupador': agrupador
+            })
+        print('Finished cleaning data. Updating internal clients table...')
+        self._update_internal_clients(
+            conn, updates)
 
     def clean(self):
         with DBManager.get_engine().connect() as conn:
